e6d914ec97341c9a9b403f16c4bb48e658727579
Reza <99068159@mydavinci.nl>
2023-05-26 15:12:11 +0200

    Heb alle gegevens op het scherm gezet en heb een beetje css toegevoegd

--- Persoonregister.js
+++ Persoonregister.js
@@ -6,6 +6,26 @@ var bijlage1 = {
     "gewicht": 86
 }
 
+var ContainerElement = document.getElementById('container');
+
+
 let voornaam = document.createElement("p");
-voornaam.innerHTML = bijlage1["voornaam"];
-document.body.appendChild(voornaam);
+voornaam.innerHTML = "voornaam: " + bijlage1["voornaam"];
+ContainerElement.appendChild(voornaam);
+
+let achternaam = document.createElement("p");
+achternaam.innerHTML = "achternaam: " + bijlage1["achternaam"];
+ContainerElement.appendChild(achternaam);
+
+let nationaliteit = document.createElement("p");
+nationaliteit.innerHTML = "nationaliteit: " + bijlage1["nationaliteit"];
+ContainerElement.appendChild(nationaliteit);
+
+let leeftijd = document.createElement("p");
+leeftijd.innerHTML = "leeftijd: " + bijlage1["leeftijd"];
+ContainerElement.appendChild(leeftijd);
+
+let gewicht = document.createElement("p");
+gewicht.innerHTML = "gewicht: " + bijlage1["gewicht"];
+ContainerElement.appendChild(gewicht);
+
